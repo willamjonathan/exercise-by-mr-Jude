d096299b7869c2585f3e4a3a248da27522364c60
will <91537837+willamjonathan@users.noreply.github.com>
2021-12-08 17:37:33 +0700

    Update CoolDriverWJM.py

--- OOP exercise/CoolDriverWJM.py
+++ OOP exercise/CoolDriverWJM.py
@@ -1,4 +1,4 @@
-from CoolShoppingWJM import SL_WJM
+from CoolClassShoppingWJM import SL_WJM
 #function that causes items to become list
 def mylist():
     orderlist = []
@@ -9,7 +9,7 @@ def mylist():
         n = eval(input("How many items will you order today?"))
     else:
             pass
-
+#creating numbers of items based on n
     for data in range(1,n+1,+1):
         print("Items #",data)
         print("please write the food names in lowercase, thank you")
@@ -18,10 +18,11 @@ def mylist():
         while pounds_Ord <= 0:
             pounds_Ord = float(input("Enter amount of pounds:"))
             
-        item =SL_WJM(food_type,pounds_Ord)      
+        item =SL_WJM(food_type,pounds_Ord)  
+ #adding the item into the list
         orderlist.append(item)
     return orderlist
-
+#function displaying the list content
 def display (orderlist):
     print("Here's a summary of the items purchased:" +"\n--------------------------")
     for data in range (len(orderlist)):
@@ -30,13 +31,13 @@ def display (orderlist):
         print("Amount ordered :", orderlist[data].pounds ,"pounds")
         print("Price per pound : $", "{:.2f}".format(orderlist[data].get_priceWJM()))
         print("Price of order : $","{:.2f}".format(orderlist[data].totalWJM())+"\n")
-        
+#function calculating total cost        
 def calc_amount(orderlist):
     total = 0
     for data in range(len(orderlist)):
         total += orderlist[data].totalWJM()
     return total
-
+#function calling function
 def main():
     all_list= mylist()
     display(all_list)
